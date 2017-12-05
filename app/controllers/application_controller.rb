5f1e63c41ba364a141c491c6506397907cd50417
Vicky Lau <vlau.nyc@gmail.com>
2017-12-04 23:50:10 -0500

    created index page and applicaiton controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,19 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security_key"
   end
 
+  get '/' do
+    @user = User.find_by_id(session[:user_id])
+    erb :index
+  end
 
+  helpers do
+    def logged_in?
+      !!current_user
+    end
 
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
 
 end #of class
