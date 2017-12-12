a702db062d594742687745abd00eb569f66909a0
Vicky Lau <vlau.nyc@gmail.com>
2017-12-12 02:17:03 -0500

    added edit and delete to controller, revised views for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,14 @@
+require 'pry'
 class CommentsController < ApplicationController
 
-
-  get '/posts/:post_id/comments' do #comments index page, off of the posts show page
-    erb :'comments/index'
-  end
+  # NOTE: Index page for comments is on the posts show page --> '/post/:id' since only comments belong to a post are shown.
   
   get '/posts/:post_id/comments/new' do #create comment
     @post = Post.find_by_id(params[:post_id])
     erb :'comments/new'
   end
 
-  post '/posts/:post_id/comments' do
+  post '/posts/:post_id/comments' do  #save the created comment
     post_id = params[:post_id]
     if params[:comment][:content] == "" 
       redirect to '/posts/#{post_id}/comments/new'
@@ -24,9 +22,10 @@ class CommentsController < ApplicationController
     redirect to "/posts/#{post_id}/comments/#{@comment.id}"
   end
 
-  get '/posts/:post_id/comments/:comment_id' do #show comment
+
+  get '/posts/:post_id/comments/:comment_id' do #show a single comment
     if logged_in?
-      # @post = Post.find_by_id(params[:post_id])
+      @post_id = params[:post_id]
       @comment = Comment.find_by_id(params[:comment_id])
       @time_ago = Time.now - @comment.updated_at
       erb :'comments/show'
@@ -35,25 +34,48 @@ class CommentsController < ApplicationController
     end
   end
 
+  get '/posts/:post_id/comments/:comment_id/edit' do #edit comment
+    if logged_in?
+      @post_id = params[:post_id]
+      @comment = Comment.find_by_id(params[:comment_id])
+      if @comment.user_id == current_user.id
+        erb :'comments/edit'
+      else
+        redirect to "/posts/#{@post_id}" #redirect to comments index
+      end
+    else
+      redirect_if_not_logged_in
+    end
+  end
 
-  # get '/posts/:id/posts/:id' do
-  #   if logged_in?
-  #     @post = Post.find_by_id(params[:id])
-  #     @time_ago = Time.now - @post.updated_at
-  #     erb :'posts/show'
-  #   else
-  #     redirect_if_not_logged_in
-  #   end
-  # end
-
-  # get '/comments/:id/edit' do #edit comment
-  # end
-
-  # patch '/comments/:id' do 
-  # end
-
-  # delete '/comments/:id/delete' do #delete comment
-  # end
-
+  patch '/posts/:post_id/comments/:comment_id' do
+    if logged_in?
+      @post_id = params[:post_id]
+      @comment = Comment.find_by_id(params[:comment_id])
+      if params[:comment][:content] == ""
+        redirect to "/posts/#{@post_id}/comments/#{@comment.id}/edit"
+      else
+        @comment.update(params[:comment])
+        @comment.save
+        redirect to "/posts/#{@post_id}/comments/#{@comment.id}"
+      end
+    else
+      redirect_if_not_logged_in
+    end
+  end
   
+  delete '/posts/:post_id/comments/:comment_id/delete' do #delete comment
+    if logged_in?
+      @post_id = params[:post_id]
+      @comment = Comment.find_by_id(params[:comment_id])
+      if @comment.user_id == current_user.id
+        @comment.delete
+        redirect to "/posts/#{@post_id}" #redirect to comments index
+      else
+        redirect to "/posts/#{@post_id}" #redirect to comments index
+      end
+    else
+      redirect_if_not_logged_in
+    end
+  end
 end #of class
